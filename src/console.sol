0da29d91a3fccb278afacfda1fc6d3334ce80e85
Christian Drappi <christiandrappi@gmail.com>
2024-08-14 13:08:10 -0400

    OK

--- src/console.sol
+++ src/console.sol
@@ -26,11 +26,11 @@ library console {
         _sendLogPayload(abi.encodeWithSignature("log(uint)", p0));
     }
     
-    function logSint(sint p0) internal view {
+    function logSint(sint256 p0) internal view {
         _sendLogPayload(abi.encodeWithSignature("log(sint)", p0));
     }
 
-    function logSuint(suint p0) internal view {
+    function logSuint(suint256 p0) internal view {
         _sendLogPayload(abi.encodeWithSignature("log(suint)", p0));
     }
 
